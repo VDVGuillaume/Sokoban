85c2337ca6fc925d4b5de5c9553db8db81202cb9
Guillaume Van de Velde <vdvguillaume@gmail.com>
2020-02-18 19:44:26 +0100

    add UC1Applicatie to Startup.java

--- src/domein/StartUp.java
+++ src/domein/StartUp.java
@@ -1,5 +1,13 @@
 package domein;
 
 public class StartUp {
+	
+	
+	
+	public static void main(String[] args) {
+		
+		DomainController domeincontroller = new DomainController();
+		UC1Applicatie app = new UC1Applicatie();
+	}
 
 }
