9e47c94d74769d4e14908a8fd84f4263cd8f98df
Van de Velde Guillaume <Guillaume.VandeVelde@digipolis.gent>
2020-02-19 20:38:04 +0100

    added code to login method in domaincontroller

--- src/domein/DomainController.java
+++ src/domein/DomainController.java
@@ -7,12 +7,25 @@ public class DomainController {
 
 	/* UC1 */
 	public void startLogIn() {
-		throw new UnsupportedOperationException();
+		
 	}
 
 	
 	/* UC1 */
 	public void logIn(String username, String password) {
+		/*GVDV LogIn method has to use the validatePasword method to retrieve the user
+		 * Not sure but i assume that we use the method in the UserRepository
+		 * Something to look at, do we need to throw exceptions for this one?
+		 * Because i did not know what to do after successful login i now just for the example show a message*/
 		
+	
+		
+		if(userRepository.validatePassword(username, password)) {
+			userRepository.retrieveUser(username);
+			System.out.print("Successful login");
+		}else {
+			System.out.print("Login failed");
+		}
+	
 	}
 }
