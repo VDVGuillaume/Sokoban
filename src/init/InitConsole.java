c1713d71c511931a460fa177fdee78654570f090
driesbols <driesbols0@gmail.com>
2020-02-20 00:58:05 +0100

    edit comment in initConsole

--- src/init/InitConsole.java
+++ src/init/InitConsole.java
@@ -4,8 +4,8 @@ import data.UserCatalog;
 
 public class InitConsole 
 {
-	/* This mini-console application is used to create some test data in the database to use when programming.
-	 * Please create database schema, database user and the required tables before initializing the db with this test data 
+	/* This mini-console application is used to create some test data in the database.
+	 * Please create the database schema, database user and the required tables before initializing the db with this test data 
 	 * You can find scripts for the above steps under src/sql_files/dll
 	 * */
 	public static void main(String[] args) 
