3fd1bd2da59b2cf09429ea8cc0177fa7c0c75e74
Van de Velde Guillaume <Guillaume.VandeVelde@digipolis.gent>
2020-02-19 20:37:02 +0100

    search now returns dummy account for testing

--- src/domein/UserMapper.java
+++ src/domein/UserMapper.java
@@ -1,9 +1,14 @@
 package domein;
 
-public class UserMapper {
 
-	public User search(String username) {
+
+public class UserMapper {
+	
+	/*GVDV Database isn't linked yet so i've made a dummy user in the mapper   */
+
+	public User search(String username) {		
 		
-		
+		 User gebruiker = new User(username,"wachtwoord",false);
+		 return gebruiker;
 	}
 }
