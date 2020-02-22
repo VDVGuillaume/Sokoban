0cf55a3fc708e01767fa646b8504e34521e87d7f
VDVGuillaume <Guillaume.vandevelde@student.hogent.be>
2020-02-22 15:50:51 +0100

    Added naam,voornaam to create_table_user.sql

--- src/sql_files/dll/create_table_user.sql
+++ src/sql_files/dll/create_table_user.sql
@@ -4,5 +4,7 @@ id int not null primary key auto_increment,
 username varchar(50) unique,
 password_hashed varchar(500),
 salt varchar(16),
-is_admin boolean
+is_admin boolean,
+naam varchar(50),
+voornaam varchar(50)
 );
