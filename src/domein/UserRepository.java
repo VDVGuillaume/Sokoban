f73b78b228da706d59012250361a39f12c84e29b
driesbols <driesbols0@gmail.com>
2020-03-02 00:08:56 +0100

    remove duplicate error handling code

--- src/domein/UserRepository.java
+++ src/domein/UserRepository.java
@@ -17,24 +17,13 @@ public class UserRepository
 		else
 		{
 			return true;
-		}
-		
-		
+		}	
 	}
 	
 	public void createUser(User user) // added name, firstName in UC2
 	{	
-		String passwordRequirement = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,}$";
-		if(user.getPassword().matches(passwordRequirement)) {
-
-
-		}else {
-			throw new PasswordException("Password doesn't meet the complexity requirements");
-		}
-
 		String salt = Security.getNextSalt();
 		String passwordHashed = Security.hash(user.getPassword(), salt.getBytes());
-		
 		
 		DbUser dbUser = new DbUser(user.getUsername(), passwordHashed, salt, user.getAdmin(), user.getName(), user.getFirstName()); 
 		
