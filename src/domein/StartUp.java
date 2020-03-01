429b89eaa93bb8ea6d94bcc4056416ecbfc55dae
MarynR <maryn.ruyloft@student.hogent.be>
2020-03-01 20:18:39 +0100

    Adjustments based on feedback

--- src/domein/StartUp.java
+++ src/domein/StartUp.java
@@ -1,16 +1,39 @@
 package domein;
 
 import ui.UC1Applicatie;
+import ui.UC2Applicatie;
+
+import java.util.Scanner;
 
 public class StartUp {
 	
-	/** UC1 StartUp class containing main method; creation of DomainController domeincontroller & UC1Applicatie app*/
+	
 	
 	public static void main(String[] args) {
 		
 		DomainController domeincontroller = new DomainController();
-		UC1Applicatie app = new UC1Applicatie(domeincontroller);
-		app.UI();
+		
+		Scanner input = new Scanner(System.in);
+		String 	username;
+		String  password;
+		
+		System.out.println("Enter 1 to log in");
+		System.out.println("Enter 2 to register");
+		int userSelection = input.nextInt();
+		switch (userSelection) { /*Added selection menu in UC2*/
+		case 1:
+			
+			UC1Applicatie app = new UC1Applicatie(domeincontroller);
+			app.UI();
+			break;
+		case 2:
+			
+			UC2Applicatie app2 = new UC2Applicatie(domeincontroller);
+			app.UI();
+			
+			break;
+		
+
 	}
 
 }
--- src/ui/UC1Applicatie.java
+++ src/ui/UC1Applicatie.java
@@ -22,11 +22,7 @@ public class UC1Applicatie {
 	String 	username;
 	String  password;
 	
-	System.out.println("Enter 1 to log in");
-	System.out.println("Enter 2 to register");
-	int userSelection = input.nextInt();
-	switch (userSelection) { /*Added selection menu in UC2*/
-	case 1:
+	
 
 		
 		try {
@@ -43,36 +39,10 @@ public class UC1Applicatie {
 			System.out.printf("%s%n",e.getMessage());		
 			UI();		
 		}
-		break;
-	case 2:
-		String 	firstName;
-		String 	name;
 		
-		try {
-		System.out.println("Give name: ");
-		input.nextLine(); //empty scanner method required to make selection screen work
-		name = input.nextLine();
-		
-		System.out.println("Give firstName: ");
-		firstName = input.nextLine();
-		System.out.println("Give username: ");
-		username = input.nextLine();
-		System.out.println("Give password: ");
-		password = input.nextLine();
-
-		
-		controller.register(name, firstName, username, password);
-		controller.logIn(username, password);
-		
-		}catch (Exception e) {
-			System.out.printf("%s%n",e.getMessage());
-			UI();
-		}
-		
-		break;
 	}
 	
 	
 	
-	}	
+		
 }
