fd0c69bcdd03ff01b9c666e13ea08a2f05809940
SDB <SDB@192.168.0.135>
2020-03-08 20:53:02 +0100

    Replace printf by println to solve error Menu

--- src/ui/Menu.java
+++ src/ui/Menu.java
@@ -11,12 +11,12 @@ public class Menu {
 	    Scanner input = new Scanner(System.in);
 	    int menuItem = 0;
 	   
-	    System.out.printf(++menuItem+". Speel spel%n");	
+	    System.out.println(++menuItem+". Speel spel%n");	
 	    if (user.getAdmin()) {
-	    System.out.printf(++menuItem+". Maak nieuw spel%n");
-	    System.out.printf(++menuItem+". Wijzig een spel%n");
+	    System.out.println(++menuItem+". Maak nieuw spel%n");
+	    System.out.println(++menuItem+". Wijzig een spel%n");
 	    }
-	    System.out.printf(++menuItem+". Afsluiten%n");
+	    System.out.println(++menuItem+". Afsluiten%n");
 	    
 	    selection = input.nextInt();
 	    return selection; 
