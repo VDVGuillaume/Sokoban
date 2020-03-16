4847f47b999712c248ab9d818bfa79cfcf49e5de
driesbols <driesbols0@gmail.com>
2020-03-16 19:04:58 +0100

    edit creation of tables for UC3

--- src/persistentie/create_table_game.sql
+++ src/persistentie/create_table_game.sql
@@ -3,3 +3,22 @@ create table GAME
 id int not null primary key auto_increment,
 name varchar(50) unique
 );
+
+create table GAMEBOARD
+(
+id	int not null primary key auto_increment,
+game_id int not null
+);
+
+alter table GAMEBOARD add foreign key (game_id) references GAME(id);
+
+create table TILE 
+(
+id	int not null primary key auto_increment,
+gameboard_id int not null,
+row_index	int not null,
+column_index int not null,
+type int not null
+);
+alter table TILE add foreign key(gameboard_id) references GAMEBOARD(id);
+alter table TILE add unique key(gameboard_id, row_index, column_index);
