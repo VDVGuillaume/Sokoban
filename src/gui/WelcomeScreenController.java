6c6249f899d5e45eab6fb1d6e11cfd6d54178f7d
driesbols <driesbols0@gmail.com>
2020-04-13 20:52:06 +0200

    add translation to GUI welcomeScreen

--- src/gui/WelcomeScreenController.java
+++ src/gui/WelcomeScreenController.java
@@ -2,11 +2,13 @@ package gui;
 
 import java.io.IOException;
 
+import domein.DomainController;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
@@ -15,7 +17,17 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 
 public class WelcomeScreenController extends GridPane {
+	private DomainController domainController;
 	private ObservableList languages;
+	
+	@FXML
+	private Label lblLogin;
+	@FXML
+	private Label lblUsername;
+	@FXML
+	private Label lblPassword;
+	@FXML
+	private Button btnRegister;
 	
 	@FXML
 	private ComboBox<String> comboBoxLanguage;
@@ -24,7 +36,8 @@ public class WelcomeScreenController extends GridPane {
 	@FXML
 	private PasswordField txtPassword;
 	
-	public WelcomeScreenController() {
+	public WelcomeScreenController(DomainController domainController) {
+		this.domainController = domainController;
 		languages = FXCollections.observableArrayList();
 		languages.addAll("English", "Français", "Nederlands");
 
@@ -36,7 +49,7 @@ public class WelcomeScreenController extends GridPane {
 		
 		try {
 			loader.load();
-//			loadData();
+			loadData();
 
 		} catch (IOException ex) {
 			throw new RuntimeException(ex);
@@ -52,6 +65,10 @@ public class WelcomeScreenController extends GridPane {
 		
 		//fill data
 		comboBoxLanguage.getItems().addAll(languages);
+		lblUsername.setText(domainController.translate("Username"));
+		lblPassword.setText(domainController.translate("Password"));
+		lblLogin.setText(domainController.translate("Login"));
+		btnRegister.setText(domainController.translate("Register"));
 	}
 	
 	@FXML
--- src/main/StartUpGui.java
+++ src/main/StartUpGui.java
@@ -1,15 +1,21 @@
 package main;
 
+import domein.DomainController;
 import gui.WelcomeScreenController;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class StartUpGui extends Application {
+	private DomainController domainController;
+
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			WelcomeScreenController root = new WelcomeScreenController();
+			domainController = new DomainController();
+			domainController.setLanguage(2);
+
+			WelcomeScreenController root = new WelcomeScreenController(domainController);
 			Scene scene = new Scene(root, 1000, 500);
 
 			primaryStage.setTitle("WelcomeScreen");
