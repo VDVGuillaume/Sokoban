cddc0c936e6142fc4aacd7564db199ff0333145a
driesbols <driesbols0@gmail.com>
2020-04-14 00:00:26 +0200

    work on GUI application

--- src/gui/WelcomeScreenController.java
+++ src/gui/WelcomeScreenController.java
@@ -8,6 +8,9 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ComboBox;
@@ -15,6 +18,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 
 public class WelcomeScreenController extends GridPane {
 	private DomainController domainController;
@@ -35,7 +39,7 @@ public class WelcomeScreenController extends GridPane {
 	private TextField txtUsername;
 	@FXML
 	private PasswordField txtPassword;
-	
+
 	@FXML
 	private Button btnLogin;
 	@FXML
@@ -54,7 +58,6 @@ public class WelcomeScreenController extends GridPane {
 	private TextField txtFirstName;
 	@FXML
 	private TextField txtName;
-	
 
 	public WelcomeScreenController(DomainController domainController) {
 		this.domainController = domainController;
@@ -82,21 +85,59 @@ public class WelcomeScreenController extends GridPane {
 		lblPassword.setText(domainController.translate("Password"));
 		lblLogin.setText(domainController.translate("Login"));
 		btnLogin.setText(domainController.translate("Login"));
-		
+
 		lblUsernameRegister.setText(domainController.translate("Username"));
 		lblPasswordRegister.setText(domainController.translate("Password"));
 		lblFirstName.setText(domainController.translate("FirstName"));
 		lblName.setText(domainController.translate("Name"));
 		btnRegister.setText(domainController.translate("Register"));
-		
+
+	}
+
+	private void loggedIn() {
+		Stage stage = (Stage) btnRegister.getScene().getWindow();
+		MenuScreenController root = new MenuScreenController(domainController);
+		Scene scene = new Scene(root, 1000, 500);
+		stage.setScene(scene);
 	}
 
 	@FXML
 	private void btnRegisterOnAction(ActionEvent event) {
+
+		var username = txtUsernameRegister.getText();
+		var password = txtPasswordRegister.getText();
+		var name = txtName.getText();
+		var firstName = txtFirstName.getText();
+
+		try {
+			domainController.register(name, firstName, username, password);
+			loggedIn();
+		} catch (Exception e) { // TODO Auto-generated catch block
+			Alert errorAlert = new Alert(AlertType.ERROR);
+			errorAlert.setHeaderText(e.getMessage());
+			//errorAlert.setContentText(e.getMessage());
+			errorAlert.showAndWait();
+		}
+	}
+	
+	@FXML
+	private void btnLoginOnAction(ActionEvent event) 
+	{
 		var username = txtUsername.getText();
 		var password = txtPassword.getText();
+		
+		try {
+			domainController.login(username, password);
+			loggedIn();
+		} catch (Exception e) { // TODO Auto-generated catch block
+			e.printStackTrace();
+			Alert errorAlert = new Alert(AlertType.ERROR);
+			errorAlert.setHeaderText(e.getMessage());
+			//errorAlert.setContentText(e.getMessage());
+			errorAlert.showAndWait();
+			}
 	}
-
+	
 	@FXML
 	private void loadLanguageChoices() {
 		int domainLanguage = domainController.getLanguage() - 1;
@@ -104,7 +145,7 @@ public class WelcomeScreenController extends GridPane {
 		comboBoxLanguage.getItems().addAll(languages);
 		comboBoxLanguage.setValue(languages.get(domainLanguage).toString());
 	}
-	
+
 	@FXML
 	private void languageChoiceChanged(ActionEvent event) {
 		int domainLanguage = domainController.getLanguage();
--- src/main/StartUpGui.java
+++ src/main/StartUpGui.java
@@ -1,6 +1,7 @@
 package main;
 
 import domein.DomainController;
+import gui.MenuScreenController;
 import gui.WelcomeScreenController;
 import javafx.application.Application;
 import javafx.scene.Scene;
@@ -8,14 +9,17 @@ import javafx.stage.Stage;
 
 public class StartUpGui extends Application {
 	private DomainController domainController;
+	private Stage primaryStage;
 
 	@Override
 	public void start(Stage primaryStage) {
 		try {
+			this.primaryStage = primaryStage;
 			domainController = new DomainController();
 			domainController.setLanguage(2);
 
 			WelcomeScreenController root = new WelcomeScreenController(domainController);
+
 			Scene scene = new Scene(root, 1000, 500);
 
 			primaryStage.setTitle("WelcomeScreen");
