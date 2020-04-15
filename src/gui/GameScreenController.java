56c3bf26235bb37173315d86a9740756e3997526
driesbols <driesbols0@gmail.com>
2020-04-15 17:59:31 +0200

    add extra translations

--- src/gui/GameScreenController.java
+++ src/gui/GameScreenController.java
@@ -5,9 +5,11 @@ import domein.DomainController;
 import javafx.event.ActionEvent;
 
 import javafx.scene.control.Label;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.ComboBox;
 
 import javafx.scene.control.Hyperlink;
@@ -24,13 +26,22 @@ public class GameScreenController extends BaseScreenController {
 	@FXML
 	private Hyperlink linkQuit;
 	@FXML
-	private Label lblTotalGameBoardsCount;
-	@FXML
 	private Label lblCompletedGameBoardsCount;
 
 	@Override
 	protected void loadData() {
-
+		String[] gameInfo = domainController.getSelectedGameInfo();
+		int gameBoardsCount = Integer.valueOf(gameInfo[0]);
+		int gameBoardsCompletedCount = Integer.valueOf(gameInfo[1]);
+		
+		lblCompletedGameBoardsCount.setText(gameBoardsCompletedCount + " " + domainController.translate("NumberGameboardsCompletedOutofTotal") + " " + gameBoardsCount);
+		linkPlayNextGameBoard.setText(domainController.translate("PlayNextGameboard"));
+		linkQuit.setText(domainController.translate("QuitGame"));
+		
+		if(gameBoardsCount == gameBoardsCompletedCount) {
+			linkPlayNextGameBoard.setDisable(true);
+			linkPlayNextGameBoard.setVisible(false);
+		}
 	}
 
 	// Event Listener on Hyperlink[#linkPlayNextGameBoard].onAction
@@ -53,6 +64,18 @@ public class GameScreenController extends BaseScreenController {
 	// Event Listener on Hyperlink[#linkQuit].onAction
 	@FXML
 	public void linkQuitOnAction(ActionEvent event) {
+		quitGame();
+	}
+	
+	private void completeGame(){
+		// show message stating that the game is won.
+		Alert informationAlert = new Alert(AlertType.INFORMATION);
+		informationAlert.setHeaderText(domainController.translate("GameCompleted"));
+		informationAlert.showAndWait();
+		quitGame();
+	}
+	
+	private void quitGame() {
 		// redirecting to previous screen
 		Stage stage = (Stage) linkQuit.getScene().getWindow();
 		GamesListScreenController root = new GamesListScreenController(domainController);
