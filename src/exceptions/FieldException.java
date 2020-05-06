4e9430714ed92b9bf2ddbc7212ad0bddd2dc036f
VDVGuillaume <Guillaume.vandevelde@student.hogent.be>
2020-05-06 20:08:14 +0200

    added FieldException class

---
+++ src/exceptions/FieldException.java
@@ -0,0 +1,34 @@
+package exceptions;
+
+public class FieldException extends RuntimeException{
+
+	public FieldException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public FieldException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public FieldException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public FieldException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public FieldException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	
+	
+	
+}
