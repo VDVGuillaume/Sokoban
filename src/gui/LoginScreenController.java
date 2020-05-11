b81516e09e5b8b483c2b751fb1c32eae02be9f8f
VDVGuillaume <Guillaume.vandevelde@student.hogent.be>
2020-05-11 21:26:27 +0200

    Changed welcomescreen

--- src/gui/LoginScreenController.java
+++ src/gui/LoginScreenController.java
@@ -1,71 +1,64 @@
 package gui;
 
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+
+import javafx.scene.control.TextField;
+import javafx.scene.control.Tooltip;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
 import domein.DomainController;
-import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 
-public class WelcomeScreenController extends BaseScreenController {
+import javafx.scene.control.PasswordField;
+
+public class LoginScreenController extends BaseScreenController {
 	@FXML
-	private Button btnNederlands;
-	@FXML
-	private Button btnEngels;
-	@FXML
-	private Button btnFrans;
-	@FXML
-	private ImageView background;
+	private Label lblLogin;
 	@FXML
 	private Label lblUsername;
 	@FXML
-	private TextField txtUsername;
-	@FXML
 	private Label lblPassword;
+	@FXML
+	private TextField txtUsername;
 	@FXML
 	private PasswordField txtPassword;
 	@FXML
-	private Label lblLoginException;
-	@FXML
 	private Button btnLogin;
-	
+	@FXML
+	private Label lblLoginException;
 
+	// Event Listener on Button[#btnLogin].onAction
 	private static String imageFilePathKey = "src\\resources\\images\\key.png";
 	
-	public WelcomeScreenController(DomainController domainController) {
-		super(domainController, "WelcomeScreen.fxml");
-		
-		
-		languages = FXCollections.observableArrayList();
-		languages.addAll("English", "Nederlands", "Français");
-		background.fitWidthProperty().bind(this.widthProperty());
-		background.fitHeightProperty().bind(this.heightProperty());		
-		
+	public LoginScreenController(DomainController domainController) {
+		super(domainController, "LoginScreen.fxml");
 	}
-	
-	
 
 	@Override
 	protected void loadData() throws FileNotFoundException {
+		// set translations
 		FileInputStream isKey = new FileInputStream(imageFilePathKey);
 		
 		Image key = new Image(isKey,15,15,false, false);
 		lblUsername.setText(domainController.translate("Username"));
 		lblPassword.setText(domainController.translate("Password"));
+		lblLogin.setText(domainController.translate("Login"));
 		btnLogin.setText(domainController.translate("Login"));
 		btnLogin.setGraphic(new ImageView(key));
+	
 		
 	}
-		
+
+
 
 	@FXML
 	private void btnLoginOnAction(ActionEvent event) {
@@ -82,6 +75,5 @@ public class WelcomeScreenController extends BaseScreenController {
 			lblLoginException.setText(e.getMessage());
 		}
 	}
-	
 	
 }
